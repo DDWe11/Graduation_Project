68b4c8329521fd654c2474556f60e48766340d39
DDWe11 <Duanjw2002@163.com>
2025-02-20 20:59:06 +0800

    update2.10

--- wastewater-management-backend/src/main/java/org/jevonD/wastewaterMS/wastewaterManagementApplication.java
+++ wastewater-management-backend/src/main/java/org/jevonD/wastewaterMS/wastewaterManagementApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("org.jevonD.wastewaterMS.modules.*.repository")
 @SpringBootApplication
 public class wastewaterManagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(wastewaterManagementApplication.class, args);
         startLoadingAnimation();
